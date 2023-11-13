f4f610789477115e49d4509753bc778d72a9863d
Reynoso <reynoso.geronimo@gmail.com>
2023-11-13 15:14:40 -0300

    refactored code, added logging

--- main.py
+++ main.py
@@ -1,66 +1,94 @@
 import time
 from datetime import datetime
+import logging
 from internet_checker import check_internet_connection
 from api_client import get_api_data, process_api_data, set_status
 from vpn_checker import check_and_connect_vpn
 from connect_sim import connect_sim
 from generate_notice import generate_notice
+
+# Configure the logging module
+logging.basicConfig(level=logging.INFO)
+
+# Create a logger with a specific name
+logger = logging.getLogger("my_logger")
+
+# Create a formatter to specify the log message format
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# Create a console handler and set the formatter
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+# Create a file handler and set the formatter
+file_handler = logging.FileHandler('error.log')
+file_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+VPN_NAME = 'Conexión Alpha2000'
+SLEEP_DURATION = 5
+
 def main():
     try:
-        vpnname = 'Conexión Alpha2000'
-        connectedCustoms = "000"
-        success = True
+        connected_customs = "000"
         start_day = datetime.now().day
 
         while True:
             check_internet_connection()
+
             response = get_api_data()
             api_data = process_api_data(response)
-            if api_data:
-                check_and_connect_vpn(vpnname)
-                print(api_data)
-                id_value = api_data['id']
-                notice = api_data['notice']
-                customsCode = api_data['customsCode']
-                fileName= api_data['fileName']
-                address= api_data['address']
-                city= api_data['city']
-                noticeDate= api_data['noticeDate']
-                noticeTime= api_data['noticeTime']
 
-                # Verificar si el día ha cambiado
+            if api_data:
+                check_and_connect_vpn(VPN_NAME)
+
+                # Process API data
+                id_value, notice, customs_code, file_name, address, city, notice_date, notice_time = (
+                    api_data['id'],
+                    api_data['notice'],
+                    api_data['customsCode'],
+                    api_data['fileName'],
+                    api_data['address'],
+                    api_data['city'],
+                    api_data['noticeDate'],
+                    api_data['noticeTime']
+                )
+
+                # Check if the day has changed
                 if start_day != datetime.now().day:
-                    connectedCustoms = "000"
+                    connected_customs = "000"
                     start_day = datetime.now().day
 
-                if connectedCustoms != customsCode:
-                    success = connect_sim(customsCode)
-                    connectedCustoms = customsCode
+                if connected_customs != customs_code:
+                    success = connect_sim(customs_code)
+                    connected_customs = customs_code
 
                 if success:
-                    succes_notice= generate_notice(fileName,address,city,noticeDate,noticeTime)
-                    if succes_notice:
-                        response = set_status(id_value,"ok",notice)
+                    success_notice = generate_notice(file_name, address, city, notice_date, notice_time)
+                    if success_notice:
+                        response = set_status(id_value, "ok", notice)
                     else:
-                        response = set_status(id_value,"error")
+                        response = set_status(id_value, "error")
+
                     if response.status_code == 200:
                         response_data = response.json()
-                        print(response_data)
+                        logger.info(response_data)
                     else:
-                        print(f'Error en la solicitud POST. Código de respuesta: {response.status_code}')
+                        logger.error(f'Error in the POST request. Response code: {response.status_code}')
                 else:
-                    print("La ejecución de connect_sim no fue exitosa. Deteniendo...")
+                    logger.error("Execution of connect_sim was not successful. Stopping...")
                     break
             else:
-                print(f'No se encontro aviso para generar. Código de respuesta: {response.status_code}')
-            
+                logger.warning(f'No notice found to generate. Response code: {response.status_code}')
 
-            time.sleep(5)
+            time.sleep(SLEEP_DURATION)
 
     except Exception as e:
-        # Captura la excepción
-        print(f"Se ha producido un error: {str(e)}")
-       
+        logger.error(f"An error occurred: {str(e)}")
+        input("Press Enter to exit...")
+
 if __name__ == "__main__":
     main()
-main()
